5ac959c9086f5e0be16d0211cdb90470b59c13f3
Vicky L <vickyl1@github.com>
2013-12-10 16:16:23 -0500

    emoji script finalized and emoji data

--- sentiment/emojicount.py
+++ sentiment/emojicount.py
@@ -1,19 +1,23 @@
 import re
 import sys
 import csv
+import numpy
 
-if len(sys.argv) < 3:
+if len(sys.argv) < 4:
   print len(sys.argv)
-  print """USAGE: python emojicount.py [input_file] [output_file]"""
+  print """USAGE: python emojicount.py [input_file] [output_file] [is_test]"""
   sys.exit()
+
+if (sys.argv[3] == "true"):
+  is_test = True
+else:
+  is_test = False
 
 emojiCountFilename = sys.argv[2]
 sep = ','
-sep2 = '\t' # must be one character so add semicolon later
 
 emojiCountf = open(emojiCountFilename, 'w')
-
-emojiCountOut = csv.writer(emojiCountf, delimiter = sep2, quotechar='', escapechar='\\', quoting=csv.QUOTE_NONE)
+emojiCountOut = csv.writer(emojiCountf, delimiter = ",", quotechar='"', escapechar='\\', quoting=csv.QUOTE_ALL)
 
 happy = '[:=]-?\)'
 sad = '[:=]-?\('
@@ -22,26 +26,38 @@ inFilename = sys.argv[1]
 f= open(inFilename, 'rU')
 fin = csv.reader(f, delimiter = sep)
 
+userToPos = {}
+userToNeg = {}
+userToNet = {}
 line = 1
 for row in fin:
   if (line == 1):
-    row.extend(["positive_emoji_count", "negative_emoji_count", "net_emoji_count"])
-    emojiCountOut.writerow(row)
     line += 1
     continue
   if len(row) == 0:
     line += 1
     continue
-  if len(row) != 6:
+  if (not(is_test) and len(row) != 6) or (is_test and len(row) != 5):
     print "Malformatted line: %i; skipping." % line
     line += 1
     continue
   text = row[1]
+  userId = row[0]
+  if not(userId in userToNet):
+    userToNet[userId] = []
+    userToPos[userId] = []
+    userToNeg[userId] = []
+  
   emojiPosCount = len(re.findall(happy, text))
   emojiNegCount = len(re.findall(sad, text))
-  row.extend([emojiPosCount, emojiNegCount, emojiPosCount - emojiNegCount])
-  emojiCountOut.writerow(row)
+  userToPos[userId].append(emojiPosCount)
+  userToNeg[userId].append(emojiNegCount)
+  userToNet[userId].append(emojiPosCount - emojiNegCount)
   line += 1
 f.close()
-  
+
+emojiCountOut.writerow(["userid","pos_emoji_count", "neg_emoji_count", "net_emoji_count"])
+for userid in userToNet.keys():
+  row = [userid,numpy.mean(userToPos[userid]),numpy.mean(userToNeg[userid]),numpy.mean(userToNet[userid])]
+  emojiCountOut.writerow(row)
 emojiCountf.close()
